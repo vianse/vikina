bac36a6c6d581875b4c2908954228fba0d2e1510
Juan Carlos Santiago <krlos013@gmail.com>
2016-12-02 11:42:27 -0600

    Cambio con anuncio

--- app/controllers/api/v1/servicios_controller.rb
+++ app/controllers/api/v1/servicios_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ServiciosController <ApplicationController
  skip_before_filter :verify_authenticity_token
 
  def create
- 
+
  servicios_params.map do |a|
        @crear_servico = Servicio.create({
 	        :titulo   => a[:titulo], 
@@ -27,7 +27,7 @@ end
 
  private
   def servicios_params
-   params.require(:servicios).map do |p|
+   params.require(:anuncio).map do |p|
    ActionController::Parameters.new(p.to_hash).permit(:titulo, :descripcion,:imagen,:lon,:lat,:direccion,:cp,:telefono,:correo, :whatsapp, :user_id, :calificacion)
    end
   end
